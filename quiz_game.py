62e3bf4c5b6fdb8a39801011c2546c52faf97af7
SRCinVA <scitarella@hotmail.com>
2021-09-10 10:12:38 -0400

    added more questions

--- quiz_game.py
+++ quiz_game.py
@@ -7,7 +7,31 @@ if playing != "yes":
 
 print("OK! Then let's play.")
 
-answer = input("What does CPU stand for? ")
-
-if answer == central processing unit:
+# Question 1
+answer1 = input("What does CPU stand for? ")
+if answer1 == "central processing unit":
     print("Nice job!")
+else:
+    print("Incorrect!")
+
+# Question 2
+# Best practice is to not re-use variables, but in this case, you could do that.
+answer2 = input("What does GPU stand for? ")
+if answer2 == "graphics processing unit":
+    print("Nice job!")
+else:
+    print("Incorrect!")
+
+# Question 3
+answer3 = input("what does RAM stand for? ")
+if answer3 == "random access memory":
+    print("Nice job!")
+else:
+    print("Incorrect!")
+
+# Question 4
+answer4 = input("What does PSU stand for? ")
+if answer4 == "power supply":
+    print("Nice job!")
+else:
+    print("Incorrect!")
